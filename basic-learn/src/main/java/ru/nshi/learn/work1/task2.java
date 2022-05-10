cd4231fdaef85e9facafebce0d4fba87723d837a
VangelNum <vangelnum@gmail.com>
2022-05-11 00:31:22 +0400

    rework

--- basic-learn/src/main/java/ru/nshi/learn/work1/task2.java
+++ basic-learn/src/main/java/ru/nshi/learn/work1/task2.java
@@ -2,15 +2,15 @@ package ru.nshi.learn.work1;
 
 /*Написать программу, которая вычисляет, сумму всех шестизначных «счастливых» чисел.*/
 
-public class task2 {
+public class Task2 {
     public static void main(String[] args){
-        task2 task_2 = new task2();
+        Task2 task_2 = new Task2();
         System.out.println(task_2.summaAllLuckyNumbers());
     }
 
     public int summaAllLuckyNumbers() {
         int sum = 0;
-        task1 task = new task1();
+        Task2 task = new Task2();
         for (int i = 100000; i <= 999999; i++){
             if (task.lucky(i) == true) {
                 sum += i;
